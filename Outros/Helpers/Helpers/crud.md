3aefde091e3d88ca7d5b8295c1120e5dd6ed9afd
Ribamar FS <ribafs@gmail.com>
2020-11-03 12:57:47 -0300

    Refatorando o crud.php

--- Outros/Helpers/Helpers/crud.md
+++ Outros/Helpers/Helpers/crud.md
@@ -17,4 +17,7 @@ return update('users', 5, ['name' => 'João Brito', 'email' => 'joao@joao.org'])
 ```php
 return delete('users', 5);
 ```
+## Agradecimento
+
+Meus sinceros agradecimentos ao colega Klaus Andrade, do grupo Laravel Brasil, que me enviou as sugestões/recomendações para refatorar as funções do crud.php. Ficaram uma beleza, ao meu ver. Daqui rpa frente tentarei fazer assim. 
 
--- Outros/Helpers/Helpers/crud.php
+++ Outros/Helpers/Helpers/crud.php
@@ -1,62 +1,85 @@
 <?php
-function select($table = 'users', $id){
 
-    // Consultar se existe o id indicado
-    $id2 = DB::table($table)->select('id')
-        ->where('id', $id)
+/**
+ * select
+ * Consultar o banco de dados e retorna o registro de acordo com o id recebido
+ * usage: return select( 3,'users' );
+ * @param  mixed $table
+ * @param  mixed $id
+ * @return void
+ */
+function select( $table = 'users', $id ): void
+{
+    $exists = DB::table( $table )->select( 'id' )
+        ->where( 'id', $id )
         ->first();
 
-    // Se não existir
-    if(is_null($id2)){
-        echo 'Não existe um user com este id: '.$id;
+    if ( $exists ) {
+        echo "Existe um registro com o id: {$id}";
+        return;
+    }
+
+    echo "Não existe um registro com o id: {$id}";
+}
+
+/**
+ * insert
+ * Inserir novo registro na tabela
+ * usage: return insert( 'users', [ 'name' => 'Ribamar FS', 'email' => 'joao@gmail.com', 'password' => bcrypt('123456')]);
+ * @param  mixed $table
+ * @param  array $fields
+ * @return void
+ */
+function insert( $table = 'users', $fields = [] )
+{
+    DB::table( $table )->insert(
+        $fields
+    );
+    echo 'Registro adicionado com sucesso ';
+}
+
+/**
+ * update
+ * Atualizar registro na tabela
+ * usage: return update( 'users', 5, [ 'name' => 'João Brito' ]);
+ * @param  mixed $table
+ * @param  mixed $whereValue
+ * @param  array $fields
+ * @return void
+ */
+function update( $table = 'users', $whereValue, $fields = [] )
+{
+    $exists = DB::table( $table )->select( 'id' )->where( 'id' , $whereValue )->first();
+
+    if ( !is_null( $exists ) ) {
+        $affected = DB::table( $table )
+              ->where( 'id', $whereValue )
+              ->update( $fields );
+        echo "Client com id: {$whereValue} atualizado com sucesso";
     }else{
-        echo 'Existe um user com este id: '.$id;
+        echo "Não existe client com este id: {$whereValue}";
     }
 }
-// Exemplo de uso: return select(3,'users');
 
-function insert($table = 'users', $fields = []){// Ex: $fields = ['name' => 'Ribamar FS', 'email' => 'ribafs@gmail.com'];
+/**
+ * delete
+ * Excluir registro da tabela
+ * usage: return delete('users', 5);
+ * @param  mixed $table
+ * @param  mixed $id
+ * @return void
+ */
+function delete( $table, $id )
+{
+    $exists = DB::table( $table )->select( 'id' )->where( 'id', $id )->first();
 
-      DB::table($table)->insert(
-          $fields
-      );
-      echo 'Registro adicionado com sucesso ';
-}
-// Exemplo de uso: return insert('users', ['name' => 'Ribamar FS', 'email' => 'joao@gmail.com', 'password' => bcrypt('123456')]);
-
-function update($table = 'users', $whereValue, $fields = []){ // Ex: 'users', ['id', 2], ['name' => 'João Brito']
-
-    // Consultar se existe o registro através do $whereValue
-    $id = DB::table($table)->select('id')->where('id', $whereValue)->first();
-
-    // Caso exista, atualizar
-    if(!is_null($id)){
-        $affected = DB::table($table)
-              ->where('id', $whereValue)
-              ->update($fields);
-        echo 'Client com id: '.$whereValue. ' atualizado com sucesso';
-    }else{
-        echo 'Não existe client com este id: '.$whereValue;
-    }
-}
-// Exemplos de uso: 
-// return update('users', 5, ['name' => 'João Brito']);
-// return update('users', 5, ['name' => 'João Brito', 'email' => 'joao@joao.org']);
-
-function delete($table, $id){
-
-    // Consultar se o registro com $id existe
-    $id2 = DB::table($table)->select('id')->where('id', $id)->first();
-
-    // Caso exista
-    if(!is_null($id2)){
-        $affected = DB::table($table)
-              ->where('id', $id)
+    if ( !is_null( $exists ) ) {
+        $affected = DB::table( $table )
+              ->where( 'id', $id )
               ->delete();
-        echo 'Registro com id: '.$id. ' excluído com sucesso';
+        echo "Registro com id: {$id} excluído com sucesso";
     }else{
-        echo 'Não existe registro com este id: '.$id;
+        echo "Não existe registro com este id: {$id}";
     }
 }
-// Exemplo de uso: return delete('users', 5);
 
